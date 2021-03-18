e67ac159ddd69e6bc4a1f71fecb3ddf8ec98f8f5
akitoshi.shiga <akitoshi.shiga@insight-inc.biz>
2021-03-18 23:59:29 +0900

    Resolved #1321

--- LeetCode/Database/medium/1321.sql
+++ LeetCode/Database/medium/1321.sql
@@ -1,19 +1,19 @@
 -- corrected
-  WITH cte as(
-      select
+  WITH cte AS(
+      SELECT
         visited_on
-        ,sum(sum(amount)) over(order by visited_on rows between 6 precending and current row)
-        ,round(avg(cast(sum(amount) as float))
-        over(order by visited_on rows between 6 precending and current row) as average_amount
-      from
+        ,SUM(SUM(amount)) OVER(ORDER BY visited_on ROWS BETWEEN 6 precending AND current ROW)
+        ,ROUND(AVG(CAST(SUM(amount) AS FLOAT))
+        OVER(ORDER BY visited_on ROWS BETWEEN 6 precending AND current ROW) AS average_amount
+      FROM
         Customer
-      group by visited_on
+      GROUP BY visited_on
   )
-  select
+  SELECT
     visited_on
     ,amount
     ,average_amount
-from
+FROM
   cte
-where
-  visited_on >= DATEADD(DAY, 6 (SELECT TOP 1 visited_on FROM cte))
+WHERE
+  visited_on >= DATEADD(DAY, 6 (SELECT TOP 1 visited_on FROM cte));
