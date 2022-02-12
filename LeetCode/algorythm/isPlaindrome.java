9cf1419e9c0977fa611b98d6b1c8fec2f8152d9e
akitoshi.shiga <akitoshi.shiga@insight-inc.biz>
2022-02-12 20:26:35 +0900

    isPalaindrome

--- LeetCode/algorythm/isPlaindrome.java
+++ LeetCode/algorythm/isPlaindrome.java
@@ -1,11 +1,17 @@
 class Solution {
     public boolean isPalindrome(int x) {
 
-        if(x < 0) return false;
-        String reversed = (String)x;
+        if(x < 0) {
+            return false;
+        }
 
+        char[] palindrome = String.valueOf(x).toCharArray();
 
-
-
+        boolean isPalindrome = true;
+        for(int i = 0; i < palindrome.length / 2; i++) {
+            isPalindrome = isPalindrome
+                        && palindrome[i] == palindrome[palindrome.length-1-i];
+        }
+        return isPalindrome;
     }
 }
